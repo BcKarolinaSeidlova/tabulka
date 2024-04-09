b734a7c24e3e76088e7c7f22443ee51070fb8403
Karolina Seidlova <35819148+BcKarolinaSeidlova@users.noreply.github.com>
2024-04-09 20:47:25 +0200

    Update csv_to_html_table.js

--- js/csv_to_html_table.js
+++ js/csv_to_html_table.js
@@ -53,7 +53,7 @@ CsvToHtmlTable = {
 
                 //$table.DataTable(datatables_options);
 
-                text.innerHTML = csvOptions;
+                text.innerHTML = csvData.length + "Boiled index: " + csvData.indexOf("Boiled");
 
                 if (allow_download) {
                     $containerElement.append("<p><a class='btn btn-info' href='" + csv_path + "'><i class='glyphicon glyphicon-download'></i> Download as CSV</a></p>");
